2d7b223e35a7fe86f260054eee624254649ab8a6
chiaramery <meringolachiara@gmail.com>
2022-12-14 13:21:51 +0100

    hello

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $greeting = "Hello world!";
+    return view('welcome', compact("greeting"));
 });
